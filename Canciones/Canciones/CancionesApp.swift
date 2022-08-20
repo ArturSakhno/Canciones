0ca6c578886fb52a982e845c1db5f59a0ad9d664
Artur Sakhno <artur_sakhno@icloud.com>
2022-08-20 15:48:17 +0200

    Project init

---
+++ Canciones/Canciones/CancionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CancionesApp.swift
+//  Canciones
+//
+//  Created by A S on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CancionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
